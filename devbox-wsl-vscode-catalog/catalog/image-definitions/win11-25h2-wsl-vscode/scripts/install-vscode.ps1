aaa8a697a0df41591e80edcf3b1738b408141c9d
Seyed Ahamed <seyed.ahamed@sainsburys.co.uk>
2025-12-17 06:41:46 +0000

    Delete devbox-wsl-vscode-catalog/catalog directory

--- devbox-wsl-vscode-catalog/catalog/image-definitions/win11-25h2-wsl-vscode/scripts/install-vscode.ps1
+++
@@ -1,4 +0,0 @@
-
-# Installs Visual Studio Code silently via WinGet
-Write-Host "Installing Visual Studio Code via WinGet..."
-winget install --id Microsoft.VisualStudioCode --silent --accept-source-agreements --accept-package-agreements
